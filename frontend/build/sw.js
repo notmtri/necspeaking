ddc4e8ab8b8c113d88ecf3342d116527cd70da70
notmtri <nguyenhoangminhtri2k8@gmail.com>
2025-11-16 14:49:16 +0700

    reset all

---
+++ frontend/build/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'speakup-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png',
+  // add more assets you want cached
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
