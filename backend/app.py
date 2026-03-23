70e6f31ac0146292ddd0049d03604c9826f86247
notmtri <nguyenhoangminhtri2k8@gmail.com>
2026-03-24 00:56:41 +0700

    port 6543

--- backend/app.py
+++ backend/app.py
@@ -49,18 +49,11 @@ else:
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(hours=2)
 
 # Database Configuration
-database_url = 'postgresql://postgres:040108Minhtri@db.uvsperhaoundcncfgvda.supabase.co:5432/postgres'
+database_url = 'postgresql://postgres.uvsperhaoundcncfgvda:040108Minhtri@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres'
 app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
     "pool_pre_ping": True,
     "pool_recycle": 300,
-    "connect_args": {
-        "host": "db.uvsperhaoundcncfgvda.supabase.co",
-        "port": 5432,
-        "gssencmode": "disable",
-        "sslmode": "require",
-        "options": "-c AddressFamily=inet"
-    }
 }
 
 db.init_app(app)
